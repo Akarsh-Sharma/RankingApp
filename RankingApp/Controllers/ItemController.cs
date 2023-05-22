382e7166cf980b2909089f3d872710c13620a128
Akarsh <akarsh311298@gmail.com>
2023-05-22 14:59:48 -0400

    Add project files.

---
+++ RankingApp/Controllers/ItemController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RankingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : Controller
+    {
+        // could store the data in a DB and call it here if this were a real world app but 
+        // for now we will just use a static collection of items
+
+    }
+}
---
+++ RankingApp/Models/ItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RankingApp.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ImageId { get; set; }
+        public int Ranking { get; set; }
+        public int ItemType { get; set; } // what kind of item is it, i.e., a movie, an album, etc.
+    }
+}
